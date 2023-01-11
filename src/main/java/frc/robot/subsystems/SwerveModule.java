8098f60862145dcac555342ea59616b3b5ec38c3
Ben Brodeur <2024bbrodeur@mynrsd.com>
2023-01-10 21:37:15 -0500

    swerve kinematics and odometry

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -1,9 +1,15 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+
 public class SwerveModule {
     
     public void set(double move, double turn) {
 
+    }
+
+    public SwerveModulePosition getPosition() {
+        return null;
     }
 
 }

37253f53250a3ca5d72cd6857830c32f81385388
0418yicheng <0418yicheng@gmail.com>
2023-01-17 15:48:26 -0500

    _

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -4,7 +4,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.RemoteFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;

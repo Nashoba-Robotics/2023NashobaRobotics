3123113b50085fc56738337b33a2600c621df72f
Yi Cheng <0418yicheng@gmail.com>
2023-05-25 13:19:05 -0400

    Fixed auto arm

--- src/main/java/frc/robot/commands/auto/intakescore/AutoPrepHighCone.java
+++ src/main/java/frc/robot/commands/auto/intakescore/AutoPrepHighCone.java
@@ -11,12 +11,12 @@ public class AutoPrepHighCone extends CommandBase{
         GrabberSubsystem.getInstance().orientPos(Constants.Grabber.PREP_CONE_FRONT_NU);
         ArmSubsystem.getInstance().pivot(Constants.Arm.HIGH_FRONT_ANGLE);
 
-        ArmSubsystem.getInstance().extendNU(Constants.Arm.HIGH_EXTEND_NU-2100);
+        ArmSubsystem.getInstance().extendNU(Constants.Arm.HIGH_EXTEND_NU-2100/2048.);
     }
 
     @Override
     public boolean isFinished() {
-        return Math.abs(ArmSubsystem.getInstance().getExtendNU()-Constants.Arm.HIGH_EXTEND_NU) < 1000
+        return Math.abs(ArmSubsystem.getInstance().getExtendNU()-Constants.Arm.HIGH_EXTEND_NU) < 1000/2048.
         &&     Math.abs(ArmSubsystem.getInstance().getPivotRad()-Constants.Arm.HIGH_FRONT_ANGLE) < 2;
     }
 }
